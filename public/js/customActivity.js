737055627eeb15f2927a8eac343e546e511d3379
miriamurgiles <miriam.urgiles@gmail.com>
2025-01-24 21:27:13 -0500

    test nuevo campo phone5

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -186,7 +186,7 @@ define([
                         "phoneNumber": "{{Contact.Attribute.TEST_FREEWAY_oportunidades_en_proceso_Whatsapp.Phone}}",
                         "emailAddress": "{{InteractionDefaults.email}}",
                         "clientName": "{{Contact.Attribute.TEST_FREEWAY_oportunidades_en_proceso_Whatsapp.Name}}",
-                        "Enlace":"https://cloud.mkcloud.grupopurdy.com/Control_de_Calidad?numCon=" + "{{Contact.Attribute.TEST_FREEWAY_oportunidades_en_proceso_Whatsapp.Opportunity:Numero_Consecutivo__c}}",
+                        "Enlace":"https://cloud.mkcloud.grupopurdy.com/Control_de_Calidad?numCon=" + "{{Contact.Attribute.TEST_FREEWAY_oportunidades_en_proceso_Whatsapp.[Opportunity:Numero_Consecutivo__c]}}",
                         "templateId": templateId
                     }];
                 }  
