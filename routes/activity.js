f6cf622487f815295f49bee6d4c5c13d84d0c212
miriamurgiles <125692050+miriamurgiles@users.noreply.github.com>
2026-06-18 15:40:41 -0500

    Update activity 18_06_ 15_40

--- routes/activity.js
+++ routes/activity.js
@@ -1,157 +1,299 @@
-'use strict';
-var util = require('util');
-
-// Deps
-const Path = require('path');
-const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
-var util = require('util');
-var http = require('https');
-
-exports.logExecuteData = [];
-
-function logData(req) {
-    exports.logExecuteData.push({
-        body: req.body,
-        headers: req.headers,
-        trailers: req.trailers,
-        method: req.method,
-        url: req.url,
-        params: req.params,
-        query: req.query,
-        route: req.route,
-        cookies: req.cookies,
-        ip: req.ip,
-        path: req.path,
-        host: req.host,
-        fresh: req.fresh,
-        stale: req.stale,
-        protocol: req.protocol,
-        secure: req.secure,
-        originalUrl: req.originalUrl
+define([
+    'postmonger'
+], function (
+    Postmonger
+) {
+    'use strict';
+    var connection = new Postmonger.Session();
+    var authTokens = {};
+    var payload = {};
+    $(window).ready(onRender);
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
+    connection.on('requestedInteraction', onRequestedInteraction);
+    connection.on('requestedTriggerEventDefinition', onRequestedTriggerEventDefinition);
+    connection.on('requestedDataSources', onRequestedDataSources);
+    connection.on('requestedSchema', function (data) {
+        // save schema
+        console.log('*** Schema ***', JSON.stringify(data['data_for_user_whats']));
     });
-    console.log("body: " + util.inspect(req.body));
-    console.log("headers: " + req.headers);
-    console.log("trailers: " + req.trailers);
-    console.log("method: " + req.method);
-    console.log("url: " + req.url);
-    console.log("params: " + util.inspect(req.params));
-    console.log("query: " + util.inspect(req.query));
-    console.log("route: " + req.route);
-    console.log("cookies: " + req.cookies);
-    console.log("ip: " + req.ip);
-    console.log("path: " + req.path);
-    console.log("host: " + req.host);
-    console.log("fresh: " + req.fresh);
-    console.log("stale: " + req.stale);
-    console.log("protocol: " + req.protocol);
-    console.log("secure: " + req.secure);
-    console.log("originalUrl: " + req.originalUrl);
-}
-
-/*
- * POST Handler for / route of Activity (this is the edit route).
- */
-exports.edit = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-    res.send(200, 'Edit');
-};
-
-/*
- * POST Handler for /save/ route of Activity.
- */
-exports.save = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-    res.send(200, 'Save');
-};
-
-/*
- * POST Handler for /execute/ route of Activity.
- */
-exports.execute = function (req, res) {
-
-    // example on how to decode JWT
-    JWT(req.body, process.env.jwtSecret, (err, decoded) => {
-
-        // verification error -> unauthorized request
-        if (err) {
-            console.error(err);
-            console.error(process.env.jwtSecret);
-            console.error(req.body.inArguments)
-            return res.status(401).end();
+    connection.on('clickedNext', save);
+    function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
+        connection.trigger('ready');
+        connection.trigger('requestTokens');
+        connection.trigger('requestEndpoints');
+        connection.trigger('requestInteraction');
+        connection.trigger('requestTriggerEventDefinition');
+        connection.trigger('requestDataSources');
+        connection.trigger('requestSchema');
+    }
+    function onRequestedDataSources(dataSources) {
+        console.log('*** requestedDataSources ***');
+        console.log(dataSources);
+    }
+    function onRequestedInteraction(interaction) {
+        console.log('*** requestedInteraction ***');
+        console.log(interaction);
+    }
+    function onRequestedTriggerEventDefinition(eventDefinitionModel) {
+        console.log('*** requestedTriggerEventDefinition ***');
+        console.log(eventDefinitionModel);
+    }
+    function initialize(data) {
+        console.log(data);
+        if (data) {
+            payload = data;
         }
-
-        if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-            
-            // decoded in arguments
-            var decodedArgs = decoded.inArguments[0];
-            console.log(decodedArgs);
-            const axios = require('axios');
-            let data = { //Mur
-              "templateId": decodedArgs.templateId,
-              "phoneNumber": decodedArgs.phoneNumber,
-              "clientName": decodedArgs.clientName,
-              "groupName": "Purdy Citas",
-              "assign": false,
-              "params": {
-                "client_name": decodedArgs.clientName
-              }
-            };
-
-            // Agrega `Enlace` en params solo si tiene un valor
-              if (decodedArgs.Enlace) {
-                data.params.Enlace = decodedArgs.Enlace;
-            }
-
-            data: JSON.stringify(data)  //Mur
-
-            let config = {
-              method: 'post',
-              maxBodyLength: Infinity,
-              url: 'https://us-central1-atomchat-io.cloudfunctions.net/templates',
-              headers: { 
-                'Content-Type': 'application/json', 
-                'Authorization': 'Bearer 5b12b8d9-20b1-1b49-762d-5ca346e02445'
-              },
-              data : data
-            };
-            
-            axios.request(config)
-            .then((response) => {
-              console.log(JSON.stringify(response.data));
-            })
-            .catch((error) => {
-              console.log(error);
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+        console.log(inArguments);
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
             });
-            logData(req);
-            res.send(200, 'Execute');
-        } else {
-            console.error('inArguments invalid.');
-            return res.status(400).end();
+        });
+        connection.trigger('updateButton', {
+            button: 'next',
+            text: 'done',
+            visible: true
+        });
+    }
+    function onGetTokens(tokens) {
+        console.log(tokens);
+        authTokens = tokens;
+    }
+    function onGetEndpoints(endpoints) {
+        console.log(endpoints);
+    }
+    function save() {
+        var templateId = $('#plantilla_what').val();
+        var templateDE = $('#plantilla_de').val();
+        if (templateDE == 'OPORTUNIDAD_LEXUS_B2C') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.OPORTUNIDAD_LEXUS_B2C.PersonMobilePhone}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.OPORTUNIDAD_LEXUS_B2C.Nombre}}",
+                "templateId": templateId
+            }];
         }
-    });
-};
-
-
-/*
- * POST Handler for /publish/ route of Activity.
- */
-exports.publish = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-    res.send(200, 'Publish');
-};
-
-/*
- * POST Handler for /validate/ route of Activity.
- */
-exports.validate = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
-    res.send(200, 'Validate');
-};
+        if (templateDE == 'LEADS_NUEVOS_B2C') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.LEADS_NUEVOS_B2C.Telefono}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.LEADS_NUEVOS_B2C.Primer_Nombre}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'DATOS_PARA_PROBAR') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.DATOS_PARA_PROBAR.Phone}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.DATOS_PARA_PROBAR.name}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'DATOS_PARA_PMP') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Nueva trayectoria - 16 de mayo de 2024 0938 - 2024-05-16T100846363.Opportunity:Account:Telefono_celular__c}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Nueva trayectoria - 16 de mayo de 2024 0938 - 2024-05-16T100846363.Opportunity:Account:FirstName}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'GP_Op_en_proceso_WA_1') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.GP_Op_en_proceso_WA_1.PersonMobilePhone}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.GP_Op_en_proceso_WA_1.AccountName}}",
+                "Enlace": "https://cloud.mkcloud.grupopurdy.com/Control_de_Calidad?numCon=" + "{{Contact.Attribute.GP_Op_en_proceso_WA_1.Numero_Consecutivo__c}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'LEADS XPENG 2024 _ XPENG Costa Rica 274053937') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.LEADS XPENG 2024 _ XPENG Costa Rica 274053937.Opportunity:Account:phone_number}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.LEADS XPENG 2024 _ XPENG Costa Rica 274053937.Opportunity:Account:full_name}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'GP_Oportunidades_2024') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.GP_Oportunidades_2024.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.GP_Oportunidades_2024.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Hino Form SF _ Hino Costa Rica 285181589') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Hino Form SF _ Hino Costa Rica 285181589.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Hino Form SF _ Hino Costa Rica 285181589.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Opp_EnProceso_WA_Feria') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Opp_EnProceso_WA_Feria.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Opp_EnProceso_WA_Feria.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Ops_UltimosSeisMeses') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Ops_UltimosSeisMeses.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Ops_UltimosSeisMeses.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Journey_OfertaUnidadSeparada') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Journey_OfertaUnidadSeparada.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Journey_OfertaUnidadSeparada.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Journey_OfertaProactiva') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Journey_OfertaProactiva.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Journey_OfertaProactiva.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Leads_G3I_2025 Xpeng') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Leads_G3I_2025 Xpeng.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Leads_G3I_2025 Xpeng.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Journey Feria Usados 1') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Journey Feria Usados 1.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Journey Feria Usados 1.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Journey Feria Usados 2') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Journey Feria Usados 2.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Journey Feria Usados 2.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Bienvenida_2025') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Bienvenida_2025.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Bienvenida_2025.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Extension Faw Meta') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Extension Faw Meta.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Extension Faw Meta.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Black Purdy Faw') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": "506" + "{{Contact.Attribute.Black Purdy Faw.Cliente personal:Móvil}}",
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Black Purdy Faw.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+        if (templateDE == 'Purdy_Notifications2') {
+            payload['arguments'].execute.inArguments = [{
+                "tokens": authTokens,
+                "phoneNumber": `506{{Contact.Attribute.Purdy_Notifications2.Cliente personal:Móvil}}`,
+                "emailAddress": "{{InteractionDefaults.email}}",
+                "clientName": "{{Contact.Attribute.Purdy_Notifications.Nombre del cliente}}",
+                "templateId": templateId
+            }];
+        }
+     if (templateDE == 'Journey_Faw_Meta_Leads') {
+    payload['arguments'].execute.inArguments = [{
+        "tokens": authTokens,
+        "phoneNumber": '{{Contact.Attribute.Journey_Faw_Meta_Leads.phone_number}}',
+        "emailAddress": "{{InteractionDefaults.email}}",
+        "clientName": "{{Contact.Attribute.Journey_Faw_Meta_Leads.full_name}}",
+        "templateId": templateId
+    }];
+}
+      if (templateDE == 'Higer_leads_form') {
+    payload['arguments'].execute.inArguments = [{
+        "tokens": authTokens,
+        "phoneNumber": '{{Contact.Attribute.Higer_leads_form.phone_number}}',
+        "emailAddress": "{{InteractionDefaults.email}}",
+        "clientName": "{{Contact.Attribute.Higer_leads_form.full_name}}",
+        "templateId": templateId
+    }];
+     }
+    
+  if (templateDE == 'Pruebas Faw') {
+    payload['arguments'].execute.inArguments = [{
+        "tokens": authTokens,
+        "phoneNumber": '{{Contact.Attribute.Pruebas_Faw.phone_number}}',
+        "emailAddress": "{{InteractionDefaults.email}}",
+        "clientName": "{{Contact.Attribute.Pruebas_Faw.full_name}}",
+        "templateId": templateId
+    }];
+     }
+       if (templateDE == 'Faw_leads') {
+    payload['arguments'].execute.inArguments = [{
+        "tokens": authTokens,
+        "phoneNumber": '{{Contact.Attribute.Faw_leads.phone_number}}',
+        "emailAddress": "{{InteractionDefaults.email}}",
+        "clientName": "{{Contact.Attribute.Faw_leads.full_name}}",
+        "templateId": templateId
+    }];
+     }
+           if (templateDE == 'Lead_Capture_Faw') {
+    payload['arguments'].execute.inArguments = [{
+        "tokens": authTokens,
+        "phoneNumber": '{{Contact.Attribute.Lead_Capture_Faw.phone_number}}',
+        "emailAddress": "{{InteractionDefaults.email}}",
+        "clientName": "{{Contact.Attribute.Faw_leads.full_name}}",
+        "templateId": templateId
+    }];
+     }
+        payload['metaData'].isConfigured = true;
+        console.log(payload);
+        connection.trigger('updateActivity', payload);
+    }
+});
